90cd6b3452cabeb5067932e4c31252c5c95ea342
hriportella <hriportella@gmail.com>
2022-09-23 13:05:48 -0400

    Added Prev Function

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -24,10 +24,17 @@ function updateSlidePosition() {
     slide.classList.add('hidden');
   });
 
+  console.log(slidePosition);
   slides[slidePosition].classList.add('visible');
 }
 
 function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+
   /*
     add an if statement here that checks
     if you're already at the max number of slides
@@ -37,6 +44,11 @@ function moveToNextSlide() {
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
   // add your code in here for when you click the "prev" button
   /*
     add an if statement here that checks
